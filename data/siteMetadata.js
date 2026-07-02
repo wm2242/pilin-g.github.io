241a06362dd6944616fb884647fca4cf5bb0ade5
wm2242 <z180640@outlook.com>
2026-07-03 00:31:22 +0800

    修改域名

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -7,7 +7,7 @@ const siteMetadata = {
   language: 'zh-CN',
   theme: 'system', // system, dark or light
   language: 'zh-CN',
-  siteUrl: 'https://pilin-g.github.io',
+  siteUrl: 'https:// guoblog.dpdns.org',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
