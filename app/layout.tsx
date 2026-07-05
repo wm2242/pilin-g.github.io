b608b553cddaa7d496f99486a6fdfa305b0bc47f
wm2242 <z180640@outlook.com>
2026-07-05 11:32:02 +0800

    chore: remove Umami tracking script from app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -94,14 +94,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
       <link rel="alternate" type="application/rss+xml" href={`${basePath}/feed.xml`} />
-
-      {/* Umami analytics script */}
-      <script
-        defer
-        src="https://umami-test.cc.cd/script.js"
-        data-website-id="9fb5818e-2610-437e-919f-12f10bdb90e4"
-      />
-
       <body className="bg-white pl-[calc(100vw-100%)] text-black antialiased dark:bg-gray-950 dark:text-white">
         <ThemeProviders>
           <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
