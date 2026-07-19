ac9cb765645050c44cad4c099e96814fe5afe974
wm2242 <z180640@outlook.com>
2026-07-19 17:39:31 +0800

    Update siteMetadata.js
    修改颜色为与系统颜色相同

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -76,9 +76,9 @@ const siteMetadata = {
       metadata: '0',
       // theme example: light, dark, dark_dimmed, dark_high_contrast
       // transparent_dark, preferred_color_scheme, custom
-      theme: 'light',
+      theme: 'preferred_color_scheme',
       // theme when dark mode
-      darkTheme: 'transparent_dark',
+      darkTheme: 'preferred_color_scheme',
       // If the theme option above is set to 'custom`
       // please provide a link below to your custom theme css file.
       // example: https://giscus.app/themes/custom_example.css
